e943eb88a274931afa65202d6afffac968dca63a
hossainahmad <hossain.ahmad38@gmail.com>
2022-03-02 16:07:56 +0600

    weather temperature added

--- js/app.js
+++ js/app.js
@@ -1,9 +1,27 @@
+// API Keys from Open Weather Map
 const API_KEY = `45bbbfc389cc3461410236ec7de9e8e6`;
+
+// Search a city and API Fetched
 
 const weatherSearch = () => {
     const cityName = document.getElementById('city-name').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`;
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => showWeather(data))
+}
+
+const setInnerText = (id, text) =>{
+    document.getElementById(id).innerText = text;
+}
+
+// Show Weather Function 
+
+const showWeather = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('temp', temperature.main.temp);
+    setInnerText('temp-condition', temperature.weather[0].main)
+
+// Set Weather Condition Name   
+    
 }
